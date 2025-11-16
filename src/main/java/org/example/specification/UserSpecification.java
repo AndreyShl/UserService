ea900f53a6ca2f61d31f222f48b7a399632f8026
AndreyShl <andrejslapkov30@gmail.com>
2025-11-16 20:46:58 +0300

    edit UserSpecification and fix NullPointer replaced null on cb.conjunction()

--- src/main/java/org/example/specification/UserSpecification.java
+++ src/main/java/org/example/specification/UserSpecification.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.domain.Specification;
 public class UserSpecification {
     public static Specification<User> firstNameContains(String name) {
         return (root, query, cb) -> name == null
-                ? null
+                ? cb.conjunction()
                 : cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%");
     }
 
     public static Specification<User> lastNameContains(String surName) {
         return (root, query, cb) -> surName == null
-                ? null
+                ? cb.conjunction()
                 : cb.like(cb.lower(root.get("surname")), "%" + surName.toLowerCase() + "%");
     }
 }
